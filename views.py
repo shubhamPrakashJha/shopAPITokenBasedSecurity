dcf53fc42d928cc72a0eb478e9fd4c96ae5f4af3
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-05-06 03:13:16 +0530

    ADD @auth.verify_password decorator to verifu username/token and password.

--- views.py
+++ views.py
@@ -17,6 +17,19 @@ app = Flask(__name__)
 
 
 # ADD @auth.verify_password decorator here
+@auth.verify_password
+def verify_password(username_or_token, password):
+	# Try to see if it's a token first
+	user_id = User.verify_auth_token(username_or_token)
+	if user_id:
+		user = session.query(User).filter_by(id=user_id).one()
+	else:
+		user = session.query(User).filter_by(
+			username=username_or_token).first()
+		if not user or not user.verify_password(password):
+			return False
+	g.user = user
+	return True
 
 
 # add /token route here to get a token for a user with login credentials
